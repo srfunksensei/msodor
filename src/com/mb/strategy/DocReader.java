9d2c44ded02251916ee36c61ef99777165a94bab
srfunksensei <sr.funk.sensei@gmail.com>
2021-02-25 10:14:36 +0100

    cleaned up DocReader

--- src/com/mb/strategy/DocReader.java
+++ src/com/mb/strategy/DocReader.java
@@ -1,5 +1,9 @@
 package com.mb.strategy;
 
+import com.mb.extractor.Extractor;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,15 +11,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.apache.poi.hwpf.HWPFDocument;
-import org.apache.poi.hwpf.extractor.WordExtractor;
-
-import com.mb.extractor.Extractor;
-
 /**
- * 
+ *
  * Class which provides algorithm for reading DOC files
- * 
+ *
  * @author Milan Brankovic
  *
  */
@@ -23,33 +22,31 @@ public class DocReader implements ReaderStrategy {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see com.mb.strategy.ReaderStrategy#readFile(java.lang.String)
 	 */
 	@Override
 	public List<String> readFile(final String fileName) {
 		final Path path = FileSystems.getDefault().getPath(fileName);
 
-		List<String> movieNames = new ArrayList<>();
+		final List<String> movieNames = new ArrayList<>();
 		try (final HWPFDocument document = new HWPFDocument(Files.newInputStream(path));
 				final WordExtractor extractor = new WordExtractor(document)) {
 			int year = -1;
-			for (String para : extractor.getParagraphText()) {
+			for (final String para : extractor.getParagraphText()) {
 				if (para != null) {
 					try {
 						year = Extractor.extractMovieYear(para);
 					} catch (Exception ex) {
 						Optional<String> movie = Extractor.extractMovieNameWithYear(para, year);
-						if(movie.isPresent()){
-							movieNames.add(movie.get());
-						}
+						movie.ifPresent(movieNames::add);
 					}
 				}
 			}
 		} catch (Exception exep) {
 			exep.printStackTrace();
 		}
-		
+
 		return movieNames;
 	}
 
