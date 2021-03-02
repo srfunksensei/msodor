b151d90666c926272420a8fd199da2c41aeec30c
srfunksensei <sr.funk.sensei@gmail.com>
2021-03-02 09:19:51 +0100

    added tests for reading doc file

--- src/main/java/com/mb/strategy/DocReader.java
+++ src/main/java/com/mb/strategy/DocReader.java
@@ -27,9 +27,16 @@ public class DocReader implements ReaderStrategy {
 	 */
 	@Override
 	public List<String> readFile(final String fileName) {
-		final Path path = FileSystems.getDefault().getPath(fileName);
-
 		final List<String> movieNames = new ArrayList<>();
+		if (fileName == null || fileName.trim().isEmpty()) {
+			return movieNames;
+		}
+
+		if (!fileName.endsWith("doc")) {
+			return movieNames;
+		}
+
+		final Path path = FileSystems.getDefault().getPath(fileName);
 		try (final HWPFDocument document = new HWPFDocument(Files.newInputStream(path));
 				final WordExtractor extractor = new WordExtractor(document)) {
 			int year = -1;
@@ -43,8 +50,8 @@ public class DocReader implements ReaderStrategy {
 					}
 				}
 			}
-		} catch (Exception exep) {
-			exep.printStackTrace();
+		} catch (Exception ex) {
+			ex.printStackTrace();
 		}
 
 		return movieNames;
---
+++ src/test/java/com/mb/strategy/DocReaderTest.java
@@ -0,0 +1,66 @@
+package com.mb.strategy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.List;
+
+/**
+ * @author Milan Brankovic
+ */
+public class DocReaderTest {
+
+    private static final String DOC_BLANK = "test_blank.doc";
+    private static final String DOC_WITH_DATA = "test_data.doc";
+
+    private final DocReader underTest = new DocReader();
+
+    @Test
+    public void readFile_blankFileName() {
+        final List<String> resultNull = underTest.readFile(null);
+        Assert.assertTrue("Expected no data for null as file path", resultNull.isEmpty());
+
+        final List<String> resultBlankString = underTest.readFile("    ");
+        Assert.assertTrue("Expected no data for blank file path", resultBlankString.isEmpty());
+    }
+
+    @Test
+    public void readFile_noSuchDocumentException() {
+        final List<String> result = underTest.readFile("non-existing.docx");
+        Assert.assertTrue("Expected no data for non existing file", result.isEmpty());
+    }
+
+    @Test
+    public void readFile_wrongType() {
+        final List<String> result = underTest.readFile("wrong_type.txt");
+        Assert.assertTrue("Expected no data for file with wrong type", result.isEmpty());
+    }
+
+    @Test
+    public void readFile_noData() throws URISyntaxException {
+        final File file = getFile(DOC_BLANK);
+
+        final List<String> result = underTest.readFile(file.getAbsolutePath());
+        Assert.assertTrue("Expected no data for empty file", result.isEmpty());
+    }
+
+    @Test
+    public void readFile_withData() throws URISyntaxException {
+        final File file = getFile(DOC_WITH_DATA);
+
+        final List<String> result = underTest.readFile(file.getAbsolutePath());
+        Assert.assertFalse("Expected data for non empty file", result.isEmpty());
+    }
+
+    private File getFile(final String fileName) throws URISyntaxException {
+        final URL resource = getClass().getClassLoader().getResource(fileName);
+        if (resource == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        }
+
+        return new File(resource.toURI());
+    }
+}
